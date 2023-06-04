d2f46e20194015541b31d2ef9f1b63b6c70129ca
Matthew Edge <medge@medgelabs.io>
2023-06-03 21:33:59 -0400

    Draft Map object

---
+++ map.go
@@ -0,0 +1,15 @@
+package oberon
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+type Map struct {
+}
+
+func (m *Map) AddSprite(s rl.Texture2D, position rl.Vector2) {
+}
+
+func (m *Map) Draw() {
+}
+
+func (m *Map) Close() {
+}
